827539398eff8a4993cc7a5612e312748a1957da
Luiz Neto <luizcsneto@outlook.com>
2024-06-19 15:46:44 -0300

    Configuração do RabbitMQ como sender no ms de order

---
+++ microservices/order/src/main/java/com/microservice/order/utils/rabbitmq/MessageSender.java
@@ -0,0 +1,16 @@
+package com.microservice.order.utils.rabbitmq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageSender {
+    
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY, message);
+    }
+}
